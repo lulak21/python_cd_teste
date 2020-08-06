180f8e98e456283e6f115290a8021c14873d2d7c
lulak21 <luiz@k21.com.br>
2020-08-06 12:52:01 -0300

    Formatação do float atualizada para a versão do Python

--- sistema_vendas/app.py
+++ sistema_vendas/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, escape, request
 
-from .calculadora_comissao import CalculadoraComissao
+from sistema_vendas.calculadora_comissao import CalculadoraComissao
 
 app = Flask(__name__)
 
-@app.route('/comissao/<float:valor_venda>')
+@app.route('/comissao/<float:venda>')
 def comissao(venda):
-    return f'{CalculadoraComissao.calcular(valor_venda)}'
+    return '{:.2f}'.format(CalculadoraComissao.calcular(venda))
--- sistema_vendas/calculadora_comissao.py
+++ sistema_vendas/calculadora_comissao.py
@@ -5,11 +5,10 @@ class CalculadoraComissao(object):
     @classmethod
     def calcular(cls, valor_venda):
         if valor_venda > 10000:
-            return valor_venda * 6 / 100
+            return valor_venda * 0.06
         else:
-            comissao = valor_venda * 5 / 100
+            comissao = valor_venda * 0.05
             # 123,456
             # (vezes 100) 12345,6
-            # (trunc) 12345
             # (dividido por 100) 123,45
-            return math.trunc(comissao * 100) / 100
+            return math.floor(comissao * 100) / 100
